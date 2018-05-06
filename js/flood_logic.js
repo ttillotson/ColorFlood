236b1a80febe0cd7eade5de98cd00ffc740ff28f
Travis Tillotson <travis.don.tillotson@gmail.com>
2018-05-06 16:59:29 -0700

    Set up Virtual Board to Handle Win-logic

--- js/flood_logic.js
+++ js/flood_logic.js
@@ -1,41 +1,27 @@
 import { board, tiles } from './grid';
 import { numRows, numCols } from './main';
 import { maxMoves, test } from './setup.js';
-import debounce from 'lodash.debounce';
 
 // Call flood when creating grid to attach initial matches
 export let moves = -1; 
 let finished = false;
 
 export function handleFlood(oldColor, newColor) {
-    if (finished || moves >= maxMoves) return;
     // Do nothing if clicked tile is original
-    if (oldColor === newColor) return;
+    // Do nothing if game is over
+    if (oldColor === newColor || finished) return;
     moves++;
-    // for (let row = 0; row < numRows; row++) {
-    //     for (let col = 0; col < numCols; col++) {
-    //         if (tiles[row][col].flooded) {
-    //             floodTile(row, col, newColor);
-    //             floodNeighbors(row, col, newColor);
-    //         }
-    //     }
-    // }
-    floodBoard(newColor);
+    floodBoard(0, 0, newColor, moves);
     floodTile(0, 0, newColor, moves);
-    // gameOver();
+    gameOver();
     updateInfo();
 }
 
-function floodBoard(newColor) {
-    for (let row = 0; row < numRows; row++) {
-        for (let col = 0; col < numCols; col++) {
-            if (board[row][col].flooded || board[row][col].color === newColor) {
-                board[row][col].flooded = true;
-                board[row][col].color = newColor;
-            }
-        }
-    }
-    gameOver();
+function floodBoard(row, col, newColor, moveId) {
+    board[row][col].flooded = true;
+    board[row][col].color = newColor;
+    board[row][col].lastChanged = moveId;
+    floodBoardNeighbors(row, col, newColor, moveId);
 }
 
 function floodTile(row, col, color, moveId) {
@@ -43,46 +29,52 @@ function floodTile(row, col, color, moveId) {
     tiles[row][col].className = color;
     tiles[row][col].lastChanged = moveId;
     tiles[row][col].flooded = true;
-    // debugger;
-    // gameOver();
-    setTimeout(floodNeighbors.bind(null, ...arguments), 30);
-    // // floodNeighbors.bind(null, ...arguments)();
+    setTimeout(floodTileNeighbors.bind(null, ...arguments), 30);
+}
+
+function floodBoardNeighbors(row, col, color, moveId) {
+    if (row < numRows - 1) toggleFlooded(row + 1, col, color, moveId);
+    if (row > 0) toggleFlooded(row - 1, col, color, moveId);
+    if (col < numCols - 1) toggleFlooded(row, col + 1, color, moveId);
+    if (col > 0) toggleFlooded(row, col - 1, color, moveId);
 }
 
 
-function floodNeighbors(row, col, color, moveId) {
+function floodTileNeighbors(row, col, color, moveId) {
     if (row < numRows - 1) canBeFlooded(row + 1, col, color, moveId);
     if (row > 0) canBeFlooded(row - 1, col, color, moveId);
     if (col < numCols - 1) canBeFlooded(row, col + 1, color, moveId);
     if (col > 0) canBeFlooded(row, col - 1, color, moveId);
 }
 
+function toggleFlooded(row, col, color, moveId) {
+    if ((board[row][col].color === color || board[row][col].flooded) && 
+    (board[row][col].lastChanged === undefined || board[row][col].lastChanged !== moveId)){
+        board[row][col].color = color;
+        board[row][col].flooded = true;
+        board[row][col].lastChanged = moveId;
+        floodBoardNeighbors(...arguments);
+    }
+}
+
+
 function canBeFlooded(row, col, color, moveId) {
-    // if (tiles[row][col].flooded) return; // Skip if it is already flooded
     if ((tiles[row][col].className === color || tiles[row][col].flooded) && 
         (tiles[row][col].lastChanged === undefined || tiles[row][col].lastChanged !== moveId)){
         floodTile(row, col, color, moveId);    // Toggle Flood
-        // setTimeout(floodNeighbors(row, col, color), 2000);   // Check the neighbors
     }
 }
 
 function floodedBoard() {
     for (let row = 0; row < numRows; row++){
         for (let col = 0; col < numCols; col++){
-            // console.log(board);
-            console.log(board[row][col].flooded);
-            console.log(`${row} ${col}`);
             if (!board[row][col].flooded) return;
-
         }
     }
-    // finished = true;
     return true;
 }
 
 function gameOver() {
-    // floodedBoard();
-    // console.log('called');
     if (floodedBoard()){
         finished = true;
         victory();
@@ -112,7 +104,6 @@ function updateInfo() {
     const infoEl = document.getElementById('info');
     const movesEl = document.getElementById('moves_counter');
     movesEl.innerHTML = moves + '/' + maxMoves;
-    // gameOver();
 }
 
 export function resetMoves() {
